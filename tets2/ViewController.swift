8a01390da9b3edbcaedcebd1e9521235ecaab866
messierr <jen-sh-en@yandex.ru>
2021-11-26 11:35:09 +0300

    Initial Commit

---
+++ tets2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tets2
+//
+//  Created by 35 кабинет on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
